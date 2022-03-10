f9ec1900bbb8b47a3623a4c7be5a8f89aa133c50
riofebrian839 <99810147+riofebrian839@users.noreply.github.com>
2022-03-10 14:22:18 +0700

    Add files via upload

---
+++ day19_java_string_token.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class day19_java_string_tokens {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        s = s.replaceAll("[!,?._'@\\s]+", " ");
+        StringTokenizer st = new StringTokenizer(s);
+        int n = st.countTokens();
+        System.out.println(n);
+        // print bisa pakai while atau for 
+        
+        while (st.hasMoreTokens()) {
+            System.out.println(st.nextToken());
+        }
+        // for(int i=0;i<n;i++){
+        //     System.out.println(st.nextToken());
+        // }
+        
+        scan.close();
+    }
+}
